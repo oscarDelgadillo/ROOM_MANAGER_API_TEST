45175e0069fbe1081acf30baa38c4647066a5e07
marcomendez <marco.mendez@fundacion-jala.org>
2018-02-05 11:00:32 -0400

    RM-008 code fixed

--- test/features/steps/smoke_get_id_services.py
+++ test/features/steps/smoke_get_id_services.py
@@ -4,9 +4,6 @@ from api_core.api_request.api_request_manager import get_delete_request
 def step_impl(context,hostname):
     context.params = {'hostname':hostname}
     response = get_delete_request(context.base_url, context.endpoint, context.method, None, None,context.params)
-    arrayId = response.text.split(",")
-    id = arrayId[0].split(':')
-    context.item_id = str(id[1]).replace('"',' ')
-
+    context.item_id = response.json()[0]['_id']
 
 
