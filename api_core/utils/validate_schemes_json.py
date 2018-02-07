4c2bf4035c4620d58d0c57c4295f2e34b66a71f6
AbnerMamani <Abner.Mamani@fundacion-jala.org>
2018-02-06 22:33:39 -0400

    fixed code

--- api_core/utils/validate_schemes_json.py
+++ api_core/utils/validate_schemes_json.py
@@ -3,13 +3,10 @@ from jsonschema.exceptions import ValidationError
 import json
 
 
-
-
-def validate_schema(json_meeting, schema ):
-    schema_ = json.loads(open('test/schemes/'+schema+'.json.').read())
+def validate_schema(json_meeting, schema):
+    schema_ = json.loads(open('test/schemes/' + schema + '.json.').read())
     try:
         validate(json_meeting, schema_)
         return True
     except ValidationError:
         return False
-
