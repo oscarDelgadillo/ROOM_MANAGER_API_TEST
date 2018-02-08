2af5054c437e080b598a889d866e84fffd98c83b
AbnerMamani <Abner.Mamani@fundacion-jala.org>
2018-02-08 14:20:10 -0400

    fixed code

---
+++ test/features/room_manager/meetings/functional/get_meetings_functional.feature
@@ -0,0 +1,32 @@
+# Author: Abner Mamani
+@smoke
+Feature: Meetings functional test
+  Validate service retrieve Meetings
+
+#  @meetings
+#  Scenario: Get Meetings
+#
+#    Given I GET to /meetings
+#    When I set the following parameters for a meeting EXTRA
+#        | owner                | start                    | credentials                 |
+#        | ''                   | 2017-04-21T20:00:00.000Z | '' |
+#      And I send the request
+#    Then I should get a response with status code 200
+#
+    @meetings
+  Scenario Outline: Get Meetings functional
+
+    Given I GET to /meetings
+    When I set the following parameters for a meeting EXTRA
+
+        | owner    | start    | end    | credentials      |
+        | <owner_> | <start_> | <end_> | <credentials_>   |
+      And I send the request
+    Then I should get a response with status code 200
+      Examples:
+        | owner_                | start_                   | end_                   | credentials_ |
+        |                       | 2017-04-21T20:00:00.000Z | 2017-04-21T21:00:00.000Z |              |
+        |                       | 2017-04-21T20:00:00.000Z | 2017-04-21T21:00:00.000Z |              |
+        |                       | 2017-04-21T20:00:00.000Z | 2017-04-21T21:00:00.000Z |              |
+        |                       | 2017-04-21T20:00:00.000Z | 2017-04-21T21:00:00.000Z |              |
+
--- test/features/steps/smoke_meetings.py
+++ test/features/steps/smoke_meetings.py
@@ -2,7 +2,53 @@ import json
 from compare import expect
 from behave import step
 from api_core.utils.validate_parameters import validate_parameters, replace_parameters
+# ----------------------------------------------------------------------
+@step(u'I set the following parameters for a meeting EXTRAaaaaaaaa')
+def step_impl(context):
+    context.params = {}
+    for row in context.table:
+        # context.params["owner"] = validate_parameters(context, row["owner"])
+        context.params["start"] = validate_parameters(context, row["start"])
+        # context.credentials = validate_parameters(context, row["credentials"])
 
+
+
+@step(u'I set the following parameters for a meeting EXTRA1aaaaaaaaaaaa')
+def step_impl(context):
+    context.params = {}
+    for row in context.table:
+        context.params["end"] = validate_parameters(context, row["end"])
+        context.params["start"] = validate_parameters(context, row["start"])
+        # context.credentials = validate_parameters(context, row["credentials"])
+
+
+@step(u'I set the following parameters for a meeting EXTRA')
+def step_impl(context):
+    print("-----------------------------------------------------------------------")
+    context.params = {}
+    for row in context.table:
+        if not row["owner"] == '':
+            context.params["owner"] = validate_parameters(context, row["owner"])
+            print(context.params["owner"])
+
+        if not row["start"] == '':
+            context.params["start"] = validate_parameters(context, row["start"])
+            print(context.params["start"])
+
+        if not row["end"] == '':
+            context.params["end"] = validate_parameters(context, row["end"])
+            print(context.params["end"])
+
+        if not row["credentials"] == '':
+            context.credentials = validate_parameters(context, row["credentials"])
+            print(context.credentials)
+        print("-----------------------------------------------------------------------")
+
+
+@when(u'I set the following parameters for a meeting {owner} {start} {end} {credentials}')
+def step_impl(context, start):
+        print(start)
+# ----------------------------------------------------------------------
 
 @step(u'I set the following parameters for a meeting')
 def step_impl(context):
